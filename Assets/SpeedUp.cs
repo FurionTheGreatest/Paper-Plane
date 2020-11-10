71476928fe380bdfe1e65705b2d6b87b7e1453cc
Furion <artemfurletov6@gmail.com>
2020-11-10 12:53:14 +0200

    speed up script

---
+++ Assets/SpeedUp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedUp : MonoBehaviour
+{
+    public static Action OnSpeedUpCollect;
+    
+    public void CollectSpeedUp()
+    {
+        OnSpeedUpCollect?.Invoke();
+    }
+}
