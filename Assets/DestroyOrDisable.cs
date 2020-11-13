e5b5291049d34be3dd1cc87056898b1a42cc9e26
Furion <artemfurletov6@gmail.com>
2020-11-13 21:03:25 +0200

    disabler for spawnables

---
+++ Assets/DestroyOrDisable.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class DestroyOrDisable : MonoBehaviour
+{
+    public bool isDestroying;
+
+    private float _lowerTreshold = -10f;
+
+    private void Update()
+    {
+        CheckForDestroyOrDisable();
+    }
+
+    private void CheckForDestroyOrDisable()
+    {
+        if (!(gameObject.transform.position.y - LevelManager.instance.currentPlayer.transform.position.y <
+              _lowerTreshold)) return;
+        if (isDestroying)
+            Destroy(gameObject);
+        else gameObject.SetActive(false);
+    }
+}
